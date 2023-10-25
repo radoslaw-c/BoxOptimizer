87f7f8fb8e20dc2ffd3350dec08cc8e437c95003
Radek <radoslaw.c@o2.pl>
2023-10-25 10:23:34 +0200

    Added layer information to Position_T

--- BoxOptimizer/inc/Position.h
+++ BoxOptimizer/inc/Position.h
@@ -7,6 +7,7 @@ public:
 	{
 		pos_width = -1;
 		pos_length = -1;
+		layer = -1;
 	}
 
 	Position_T(float width, float length)
@@ -14,8 +15,17 @@ public:
 		pos_width = width;
 		pos_length = length;
 	}
+
+	Position_T(float width, float length, short layer)
+	{
+		pos_width = width;
+		pos_length = length;
+		this->layer = layer;
+	}
+
 	float pos_width, pos_length;
+	short layer = 0;
 };
 
-bool operator== (Position_T pos1, Position_T pos2);
-bool operator!= (Position_T pos1, Position_T pos2);
+bool operator== (const Position_T& pos1, const Position_T& pos2);
+bool operator!= (const Position_T& pos1, const Position_T& pos2);
--- BoxOptimizer/src/Position.cpp
+++ BoxOptimizer/src/Position.cpp
@@ -1,11 +1,12 @@
 #include "Position.h"
 
-bool operator== (Position_T pos1, Position_T pos2)
+bool operator== (const Position_T& pos1, const Position_T& pos2)
 {
-	return pos1.pos_length == pos2.pos_length && pos1.pos_width == pos2.pos_width;
+	return pos1.pos_length == pos2.pos_length && pos1.pos_width == pos2.pos_width &&
+		pos1.layer == pos2.layer;
 }
 
-bool operator!= (Position_T pos1, Position_T pos2)
+bool operator!= (const Position_T& pos1, const Position_T& pos2)
 {
 	return !(pos1 == pos2);
 }
