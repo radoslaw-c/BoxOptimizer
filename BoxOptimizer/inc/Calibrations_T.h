88df7065ebe5804a52b1b47f715f5ade4b53f88c
Radek <radoslaw.c@o2.pl>
2023-10-29 18:01:16 +0100

    Added calibrations for 3D case
    For future use

--- BoxOptimizer/inc/Calibrations_T.h
+++ BoxOptimizer/inc/Calibrations_T.h
@@ -3,5 +3,7 @@ class Calibrations_T
 {
 public:
 	const float maxAllowedArea = 1.5; //How much can area of solution be greater than sum of areas of elements
-
+	
+	const float layerTolerance_down = 0.2;	//
+	const float layerTolerance_up = 0.2;
 };
