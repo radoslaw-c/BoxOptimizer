e80aeb1ccde155fcb46a15ef3e5a6f0ef9b975cf
Radek <radoslaw.c@o2.pl>
2023-12-11 18:55:45 +0100

    Drawing primitives works from class as expected

--- BoxOptimizer/src/GUI/screenObjects/Primitive_T.cpp
+++ BoxOptimizer/src/GUI/screenObjects/Primitive_T.cpp
@@ -14,33 +14,10 @@ Primitive_T::~Primitive_T()
 
 void Primitive_T::Initialize()
 {
-	//CreateBuffers();
-	//InitializeVertexArray();
-	//InitializeBuffers();
-
-	// TODO maybe some cleanup later on? 
-	glGenVertexArrays(1, &vertexArray);
-	glBindVertexArray(vertexArray);
-
-	glGenBuffers(1, &arrayBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), vertexData.data(), GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glGenBuffers(1, &elementArrayBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementArrayBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer.size() * sizeof(unsigned int),
-		elementBuffer.data(),
-		GL_STATIC_DRAW);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	CreateBuffers();
+	InitializeBuffers();
+	InitializeVertexArray();
+	UnbindBuffers();
 }
 
 void Primitive_T::Draw()
@@ -70,7 +47,7 @@ void Primitive_T::InitializeVertexArray()
 	glEnableVertexAttribArray(0);
 
 	// vertex data -- color
-	// NOTE: may need adding alfa channel
+	// TODO: may need adding alfa channel
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 
 		(void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
@@ -84,8 +61,6 @@ void Primitive_T::InitializeBuffers()
 		vertexData.data(), GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer.size() * sizeof(unsigned int),
 		elementBuffer.data(), GL_STATIC_DRAW);
-
-	UnbindBuffers();
 }
 
 void Primitive_T::BindBuffers()
